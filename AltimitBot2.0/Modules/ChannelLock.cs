fe57bb969d30b74f6cc11c21654cd5ad07323a0a
Haceo <shetah1@yahoo.com>
2020-02-04 05:22:42 -0500

    added error handling to ChannelLock

--- AltimitBot2.0/Modules/ChannelLock.cs
+++ AltimitBot2.0/Modules/ChannelLock.cs
@@ -12,8 +12,22 @@ namespace AltimitBot2._0.Modules
     {
         [Command("lock", RunMode = RunMode.Async)]
         [RequireUserPermission(GuildPermission.ManageChannels)]
-        public async Task LockChannel(string emote, string role, string msg)
+        public async Task LockChannel(string emote, string role, [Remainder]string msg = "")
         {
+            var cLock = BotConfig.LockList.FirstOrDefault(x => x.Channel == Context.Channel.Id && x.Server == Context.Guild.Id) ?? null;
+            if (cLock != null)
+            {
+                await Misc.EmbedWriter(Context.Channel, Context.User, "Error!", "This channel has already been locked, please use a different channel!");
+                return;
+            }
+            var checkRole = Context.Guild.Roles.FirstOrDefault(x => x.Name == role) ?? null;
+            if (checkRole == null)
+            {
+                await Misc.EmbedWriter(Context.Channel, Context.User, "Error!", "Role not found or does not exist as typed!");
+                return;
+            }
+            if (msg == "")
+                msg = "React with " + emote + " to gain role " + role;
             await Context.Channel.DeleteMessageAsync(Context.Message);
             LockChannel newLock = new LockChannel();
             newLock.Channel = Context.Channel.Id;
@@ -31,7 +45,13 @@ namespace AltimitBot2._0.Modules
         [RequireUserPermission(GuildPermission.ManageChannels)]
         public async Task UnlockChannel()
         {
-            var message = BotConfig.LockList.FirstOrDefault(x => x.Channel == Context.Channel.Id && x.Server == Context.Guild.Id).Message;
+            LockChannel channel = BotConfig.LockList.FirstOrDefault(x => x.Channel == Context.Channel.Id && x.Server == Context.Guild.Id) ?? null;
+            if (channel == null)
+            {
+                await Misc.EmbedWriter(Context.Channel, Context.User, "Error!", "This channel does not contain a lock!");
+                return;
+            }
+            ulong message = channel.Message;
             await Context.Channel.DeleteMessageAsync(Context.Message);
             await Context.Channel.DeleteMessageAsync(messageId: message);
             BotConfig.LockList.Remove(new LockChannel { Message = message });
